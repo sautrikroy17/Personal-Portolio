f04ddfa6071bae1dadacd107ebddaf7ef2fbd2cc
Sautrik <sr9973@srmist.edu.in>
2026-07-24 23:31:49 +0530

    Optimize design: remove LiquidRipple for a professional look

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Experience from "./components/sections/Experience";
 import Contact from "./components/sections/Contact";
 import Preloader from "./components/ui/Preloader";
 import CommandPalette from "./components/ui/CommandPalette";
-import LiquidRipple from "./components/ui/LiquidRipple";
 import InteractiveGrid from "./components/ui/InteractiveGrid";
 import ScrollProgressRing from "./components/ui/ScrollProgressRing";
 import SpatialWrapper from "./components/ui/SpatialWrapper";
@@ -19,8 +18,6 @@ function App() {
 
   return (
     <div className="relative min-h-screen font-sans text-slate-100 selection:bg-cyan-500/30 bg-slate-950 flex flex-col overflow-x-hidden">
-      
-      <LiquidRipple />
       <CommandPalette />
       <ScrollProgressRing />
       
